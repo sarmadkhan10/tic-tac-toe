96300fa7bcb3fddbfa366195e32c1a8be1642469
sarmadkhan10 <11beemskhan@seecs.edu.pk>
2016-08-21 23:25:39 +0500

    Committing the finished website (technical aspect)
    -Added highscore table
    -Completed gameplay functionality.

--- client/main.js
+++ client/main.js
@@ -2,74 +2,123 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import './highscores.html';
 
-var board_data = new Array(3);
-const pos_x = 5;
-const pos_y = 7;
+Players = new Mongo.Collection("players");
 
-/*
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const board_size = 3;
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+var board_data = new Array(board_size);
+const index_x = 5;
+const index_y = 7;
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-*/
+var game_over = false;
+var player1 = "";
+var player2 = "";
 
-Template.tic_tac_toe.onCreated(function helloOnCreated() {
- 	for (var i = 0; i <= 2; i++) {
+Template.tic_tac_toe.onCreated(function onCreated() {
+ 	for (var i = 0; i <= 2; i++)
   		board_data[i] = new Array(3);
-	}
 
 	for (var y = 0; y <= 2; y++)
 		for (var x = 0; x <= 2; x++)
 			board_data[y][x] = 2;
 
-	Session.set("player_no", 1);
+	Session.set("current_player", 1);
 });
 
-Template.play_template.helpers({
- 	player: function() {
- 		return Session.get("player_no");
- 	},
+Template.tic_tac_toe.helpers({
 });
 
 Template.tic_tac_toe.events({
 	'click .rectangle'(event) {
-    	console.log(event.target.id);
-    	markX(event.target);
+    	if (game_over == false)
+    	{
+	    	if(Session.get("current_player") == 1)
+	    	{
+	    		markX(event.target);
+	    		if (game_over == true)
+	    			updateScore(player1);
+	    		else
+	    			Session.set("current_player", 2);
+	    	}
+	    	else
+	    	{
+	    		markO(event.target);
+	    		if (game_over == true)
+	    			updateScore(player2);
+	    		else
+	    			Session.set("current_player", 1);
+	    	}
+	    }
+	},
+});
+
+Template.play_template.onCreated(function onCreated() {
+	Session.set("result", "Game in progress.");
+});
+
+Template.play_template.helpers({
+	result: function() {
+		return "Winner: " + Session.get("result");
+	},
+
+	player: function() {
+		if (Session.get("current_player") == 1)
+ 			return player1;
+ 		else
+ 			return player2;
+ 	},
+
+ 	gameStarted: function() {
+ 		return Session.get("usernamesEntered");
+ 	}
+});
+
+Template.usernames_template.helpers({
+});
+
+Template.usernames_template.events({
+	'submit .js_usernames'(event) {
+		event.preventDefault();
+
+		player1 = event.target.player1.value;
+		player2 = event.target.player2.value;
+
+		Meteor.call("insertPlayer", player1, player2);
+
+		Session.set("usernamesEntered", true);
+	},
+});
+
+Template.highscores_template.onCreated(function onCreated() {
+	Meteor.subscribe("players");
+});
+
+Template.highscores_template.helpers({
+	scoreTable: function() {
+		return Players.find();
 	},
 });
 
 markO = function(element) {
 	element.innerHTML = "O";
 
-	var x_index = parseInt(element.id[pos_x] - 1);
-	var y_index = parseInt(element.id[pos_y] - 1);
-	board_data[y_index][x_index] = 0;
+	var x_index = parseInt(element.id[index_x] - 1);
+	var y_index = parseInt(element.id[index_y] - 1);
+	board_data[y_index][x_index] = "O";
 
-	printArray();
+	checkBoardSate(x_index, y_index, "O");
 }
 
 markX = function(element) {
 	element.innerHTML = "X";
 	
-	var x_index = parseInt(element.id[pos_x] - 1);
-	var y_index = parseInt(element.id[pos_y] - 1);
-	board_data[y_index][x_index] = 1;
+	var x_index = parseInt(element.id[index_x] - 1);
+	var y_index = parseInt(element.id[index_y] - 1);
+	board_data[y_index][x_index] = "X";
 
-	printArray();
+	checkBoardSate(x_index, y_index, "X");
 }
 
 printArray = function() {
@@ -80,4 +129,58 @@ printArray = function() {
 		console.log(" ");
 	}
 
+}
+
+checkBoardSate = function(x, y, state) {
+	// check rows
+	for (var i = 0; i<3; i++)
+	{
+		if (board_data[y][i] != state)
+			break;
+		if (i == board_size-1)
+			gameOver();
+	}
+
+	// check columns
+	for (var i = 0; i<3; i++)
+	{
+		if (board_data[i][x] != state)
+			break;
+		if (i == board_size-1)
+			gameOver();
+	}
+
+	// check diagonal and anti-diagonal
+	if ((x === y) || ((board_size -1 - y) === x) || ((board_size -1 - x) === y))
+	{
+		for (var i=0; i<3; i++)
+		{
+			if (board_data[i][i] != state)
+				break;
+			if (i === board_size-1)
+				gameOver();
+		}
+
+		for (var i=0; i<3; i++)
+		{
+			if (board_data[i][(board_size-1)-i] != state)
+				break;
+			if (i === board_size-1)
+				gameOver();
+		}
+
+	}
+
+}
+
+gameOver = function(state) {
+	if (Session.get("current_player") == 1)
+		Session.set("result", player1);
+	else
+		Session.set("result", player2);
+	game_over = true;
+}
+
+updateScore = function(winner) {
+	Meteor.call("updateScoreBoard", winner);
 }
--- server/main.js
+++ server/main.js
@@ -1,5 +1,48 @@
 import { Meteor } from 'meteor/meteor';
 
+Players = new Mongo.Collection("players");
+
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.publish("players", function publishPlayers() {
+	return Players.find();
+});
+
+Meteor.methods({
+	'insertPlayer': function(playerName1, playerName2){
+		var result = Players.findOne({name: playerName1});
+
+		// if the name is not found, insert it in the database with a score of 0
+		if(!result)
+		{
+			Players.insert({name: playerName1, score: 0});
+		}
+
+		result = Players.findOne({name: playerName2});
+
+		// if the name is not found, insert it in the database with a score of 0
+		if(!result)
+		{
+			Players.insert({name: playerName2, score: 0});
+		}
+	},
+
+	'updateScoreBoard': function(playerName){
+		var prevScore;
+
+		var result = Players.findOne({name: playerName});
+
+		if(result)
+		{
+			prevScore = result.score;
+
+			Players.update(
+				{"name": playerName},
+				{$set: {"score": prevScore+1}}
+			);
+		}
+	},
+
 });
